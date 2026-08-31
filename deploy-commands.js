8e34fdb55e591923a5b61bc0d491f4a58bb80e2c
MrEz <39240221+Zudin987@users.noreply.github.com>
2026-08-31 21:29:59 +0800

    Handle Discord command deployment failures cleanly

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,29 @@
 import { REST, Routes } from 'discord.js';
 import { commands } from './src/commands.js';
 import { config } from './src/config.js';
+import { describeDiscordDeployError } from './src/deploy-errors.js';
 
 const rest = new REST({ version: '10' }).setToken(config.token);
 const body = commands.map((command) => command.data.toJSON());
 
-if (config.guildId) {
-  console.log(`Deploying ${body.length} command(s) to test guild ${config.guildId}...`);
-  await rest.put(
-    Routes.applicationGuildCommands(config.clientId, config.guildId),
-    { body }
-  );
-} else {
-  console.log(`Deploying ${body.length} command(s) globally...`);
-  await rest.put(Routes.applicationCommands(config.clientId), { body });
-}
+try {
+  if (config.guildId) {
+    console.log(`Deploying ${body.length} command(s) to test guild ${config.guildId}...`);
+    await rest.put(
+      Routes.applicationGuildCommands(config.clientId, config.guildId),
+      { body }
+    );
+  } else {
+    console.log(`Deploying ${body.length} command(s) globally...`);
+    await rest.put(Routes.applicationCommands(config.clientId), { body });
+  }
 
-console.log('Commands deployed successfully.');
+  console.log('Commands deployed successfully.');
+} catch (error) {
+  console.error('');
+  console.error(describeDiscordDeployError(error, {
+    clientId: config.clientId,
+    guildId: config.guildId
+  }));
+  process.exitCode = 1;
+}
